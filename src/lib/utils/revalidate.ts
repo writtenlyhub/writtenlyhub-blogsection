867f7ba25bfbb241da6daf327124332ed44bbe38
vrshraj <iamvrsh@gmail.com>
2026-07-23 15:11:17 +0530

    fix: automatic fallback to VERCEL_URL for webhook cache revalidation

--- src/lib/utils/revalidate.ts
+++ src/lib/utils/revalidate.ts
@@ -1,7 +1,13 @@
 export function revalidateCollection(tags: string[]) {
   // Fire and forget fetch request to our Next.js Route Handler.
   // We use full absolute URL since this may be running in the server context.
-  const baseUrl = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000';
+  const vercelUrl = process.env.VERCEL_PROJECT_PRODUCTION_URL 
+    ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}` 
+    : process.env.VERCEL_URL 
+      ? `https://${process.env.VERCEL_URL}` 
+      : null;
+      
+  const baseUrl = process.env.NEXT_PUBLIC_SERVER_URL || vercelUrl || 'http://localhost:3000';
   
   fetch(`${baseUrl}/api/revalidate`, {
     method: 'POST',
